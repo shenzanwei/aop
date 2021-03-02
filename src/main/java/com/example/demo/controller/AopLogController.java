6ce07daef4d6356379319b239e3818465246e59b
z.shen <n.shen@sjtu.edu.cn>
2021-03-02 16:12:14 +0800

    diyici xuanze git

---
+++ src/main/java/com/example/demo/controller/AopLogController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author longzhonghua
+ * @data 2019/02/03 09:50
+ */
+@RestController
+public class AopLogController {
+    @GetMapping("/aoptest")
+    public String aVoid(){
+        //int i = 4/0;
+        return "hello aop test";
+    }
+}
